8228f09e5cd658f9c5bead45af041ce96eee70fb
VitorPaulon <vtrpaulon@gmail.com>
2025-10-15 06:55:24 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_dotnet.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Pessoas> Pessoas { get; set; } = default!;
+    public DbSet<Cursos> Cursos { get; set; } = default!;
+}
+
+public class Cursos
+{
+}
+
+public class Pessoas
+{
+}
